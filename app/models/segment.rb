ac75e5b1b0bd73332d1cf90bbaaf2725aa13e3da
sert <demo.sert@gmail.com>
2015-10-12 16:06:29 +0000

    add has_many belongs_to

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,2 +1,4 @@
 class Segment < ActiveRecord::Base
+  has_many :segment_tags
+  has_many :tags, through: :segment_tags
 end
--- app/models/segment_tag.rb
+++ app/models/segment_tag.rb
@@ -1,2 +1,4 @@
 class SegmentTag < ActiveRecord::Base
+  belongs_to :segment
+  belongs_to :tag
 end
