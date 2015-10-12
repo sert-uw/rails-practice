b4e3c9db53b158b39d24af6eaf5960907ecf8c51
sert <demo.sert@gmail.com>
2015-10-12 09:30:35 +0000

    create visitors view

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,43 @@
 class VisitorsController < ApplicationController
-  protect_from_forgery except: [:create]
+  protect_from_forgery except: [:index, :new, :show, :create]
 
-  def create
-    p params
-    WebsocketRails[:visitor].trigger :visitor, params
-    render json: {'message': 'OK'}
+  before_action :set_visitor, only: [:show]
+
+  def index
+    @visitors = Visitor.all
   end
+
+  def new
+    @visitor = Visitor.new
+  end
+
+  def show
+  end
+
+  # POST /visitors
+  # POST /visitors.json
+  def create
+    @visitor = Visitor.new(visitor_params)
+
+    respond_to do |format|
+      if @visitor.save
+        format.html { redirect_to @visitor, notice: 'Visitor was successfully created.' }
+        format.json { render :show, status: :created, location: @visitor }
+      else
+        format.html { render :new }
+        format.json { render json: @visitor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_visitor
+      @visitor = Visitor.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def visitor_params
+      params.require(:visitor).permit(:sex, :age)
+    end
 end
