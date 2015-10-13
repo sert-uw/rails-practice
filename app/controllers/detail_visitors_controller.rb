267e289c9c910cd7973e88117b4255ed2b207642
sert <demo.sert@gmail.com>
2015-10-13 06:00:50 +0000

    create detail_visitors_controller

--- app/controllers/detail_visitors_controller.rb
+++ app/controllers/detail_visitors_controller.rb
@@ -1,2 +1,11 @@
 class DetailVisitorsController < ApplicationController
+  before_action :set_visitor
+
+  def index
+  end
+
+  private
+    def set_visitor
+      @visitor = Visitor.find(params[:visitor_id])
+    end
 end
